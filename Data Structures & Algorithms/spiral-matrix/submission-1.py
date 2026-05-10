ad1f4b7e2e6ed38db4867fc907f48080b7624547
rohit0710 <rohitvibhuspam@gmail.com>
2026-05-10 01:27:15 -0500

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def spiralOrder(self, grid ) -> List[int]:
+        m,n = len(grid), len(grid[0])
+        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        cur_dir, change_dir = 0, 0
+        res = [grid[0][0]]
+        vis = "visited"
+        grid[0][0] = vis
+        i,j = 0, 0
+        while change_dir < 2:
+            while True:
+                ni = i + dir[cur_dir][0]
+                nj = j + dir[cur_dir][1]
+
+                if not 0 <= ni < m or not 0 <= nj < n or grid[ni][nj] == vis:
+                    break
+                
+                change_dir = 0
+                res.append(grid[ni][nj])
+                grid[ni][nj] = vis
+
+                i = ni
+                j = nj
+            
+            cur_dir = (cur_dir + 1) % 4
+            change_dir += 1
+        return res
