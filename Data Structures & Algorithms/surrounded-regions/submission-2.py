e3f5fcf3f4755002c22867dcf023ec006d81e9c4
rohit0710 <rohitvibhuspam@gmail.com>
2026-06-24 23:12:05 -0500

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solve(self, grid: List[List[str]]) -> None:
+        m, n = len(grid), len(grid[0])
+        dir = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+        def dfs(i , j):
+            if 0 <= i < m and 0 <= j < n and grid[i][j] == "O":
+                grid[i][j] = "S"
+                for d in dir:
+                    ni, nj = i + d[0], j + d[1]
+                    dfs(ni, nj)
+        
+        for i in range(m):
+            if grid[i][0] == "O":
+                dfs(i, 0)
+            if grid[i][n-1] == "O":
+                dfs(i, n-1)
+        
+        for j in range(n):
+            if grid[0][j] == "O":
+                dfs(0, j)
+            if grid[m-1][j] == "O":
+                dfs(m-1, j)
+        
+        # print(grid)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "O":
+                    grid[i][j] = "X"
+                if grid[i][j] == "S":
+                    grid[i][j] = "O"
